99c55104fb25231e3f4123c03ed442f82f32ed63
yamada-jikennya <92251718+yamada-jikennya@users.noreply.github.com>
2021-10-13 13:26:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let page = location.search.split("&")[0].split("=")[1];
 let request = new XMLHttpRequest();
 if(page==="main"){
-  request.open('GET',static/main.xml,true);
+  request.open('GET',"static/main.xml",true);
 }
 request.onreadystatechange=function(){
   if(request.readyState===4&&request.status===200){
     document.getElementByID("main").innerHTML = request.responseXML.getElementsByTagName("pages");
   }
 }
+request.send();
