2fd94ba48202ed9862054f318c4b1a1e543dcf73
yamada-jikennya <92251718+yamada-jikennya@users.noreply.github.com>
2021-10-16 09:22:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 let page = location.search.split("&")[0].split("=")[1];
 let request = new XMLHttpRequest();
 request.responcseType = "xml"
-if(page==="main"){
-  request.open('GET',"static/main.xml",true);
-}
+request.open('GET',"static/"+page+".xml",true);
 request.onreadystatechange=function(){
   if(request.readyState===4&&request.status===200){
     document.getElementById("main").innerHTML = request.responseText;
